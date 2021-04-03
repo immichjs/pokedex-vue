afa0f896a6fde9317bd33dbcd22b595d751544a8
Michel França <fr.mich17@gmail.com>
2021-04-03 17:00:37 -0300

    Remoção de resto de lixo encontrado pelo código

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
   <div id="app">
     <header class="header">
-      <img src="../public/pokedex-icon.webp">
-      <input class="search" type="text" placeholder="Buscar pokemon" v-model="search">
+      <img src="../public/pokedex-icon.webp" />
+      <input
+        class="search"
+        type="text"
+        placeholder="Buscar pokemon"
+        v-model="search"
+      />
     </header>
 
     <div class="container-box">
-      <div v-for="(poke) in searchResult" :key="poke.url">
-        <Pokemon :name="poke.name" :url="poke.url"/>
+      <div v-for="poke in searchResult" :key="poke.url">
+        <Pokemon :name="poke.name" :url="poke.url" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import Pokemon from './components/Pokemon'
+import axios from 'axios';
+import Pokemon from './components/Pokemon';
 
 export default {
   name: 'App',
@@ -24,100 +29,95 @@ export default {
     return {
       pokemons: [],
       search: '',
-      // fadeIn: false
-    }
+    };
   },
 
   created() {
-    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(response => {
-      console.log('Lista de pokemons concluído')
-      this.pokemons = response.data.results
-    })
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+      .then((response) => {
+        console.log('Lista de pokemons concluído');
+        this.pokemons = response.data.results;
+      });
   },
 
   components: {
-    Pokemon
+    Pokemon,
   },
-
-  // methods: {
-  //   fadeInClass($event) {
-  //     console.log($event)
-  //     setTimeout(function () {
-  //       this.fadeIn = !this.fadeIn
-  //     }, 800);
-  //   },
-  // },
 
   computed: {
     searchResult() {
       if (this.search == '' || this.search == ' ') {
-        return this.pokemons
+        return this.pokemons;
       } else {
-        return this.pokemons.filter(pokemon => pokemon.name.match(this.search))
+        return this.pokemons.filter((pokemon) =>
+          pokemon.name.match(this.search)
+        );
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="postcss">
+.header {
+  @apply flex justify-center flex-wrap gap-10 bg-gray-700 mb-3;
+  box-shadow: 0 2px 5px -5px rgba(0, 0, 0, 0.2);
+}
+
+.header img {
+  @apply w-20 justify-self-center my-3;
+}
+
+.search {
+  @apply p-3.5 rounded-md border w-80 my-3 flex text-center self-center;
+  outline: none;
+}
+
+.search::placeholder {
+  @apply font-medium;
+}
+
+.container-box {
+  @apply flex flex-wrap mx-20 justify-center mb-3;
+}
+
+@media only screen and (max-width: 1024px) {
+  .container-box {
+    @apply mx-9;
+  }
+}
+
+@media only screen and (max-width: 500px) {
   .header {
-    @apply flex justify-center flex-wrap gap-10 bg-gray-700 mb-3;
-    box-shadow: 0 2px 5px -5px rgba(0, 0, 0, .2);
+    @apply gap-3;
   }
 
   .header img {
-    @apply w-20 justify-self-center my-3;
+    @apply w-16;
   }
 
   .search {
-    @apply p-3.5 rounded-md border w-80 my-3 flex text-center self-center;
-    outline: none;
-  }
-
-  .search::placeholder {
-    @apply font-medium;
+    @apply w-60;
   }
 
   .container-box {
-    @apply flex flex-wrap mx-20 justify-center mb-3;
+    @apply mx-0;
+  }
+}
+
+@media only screen and (max-width: 315px) {
+  .header img,
+  .search {
+    @apply my-0;
   }
 
-  @media only screen and (max-width: 1024px) {
-    .container-box {
-      @apply mx-9;
-    }
+  .header img {
+    @apply mt-3;
   }
 
-  @media only screen and (max-width: 500px) {
-    .header {
-      @apply gap-3;
-    }
-    
-    .header img {
-      @apply w-16;
-    }
-
-    .search {
-      @apply w-60;
-    }
-
-    .container-box {
-      @apply mx-0;
-    }
+  .search {
+    @apply mb-3;
   }
-  
-  @media only screen and (max-width: 315px) {
-    .header img, .search {
-      @apply my-0;
-    }
-
-    .header img {
-      @apply mt-3;
-    }
-
-    .search {
-      @apply mb-3;
-    }
-  }
+}
 </style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,87 +1,86 @@
 <template>
-    <div class="card" @click="changeImage">
-        <img class="pokemon-image" :src="currentImage">
-        <div class="container">
-            <h1>{{ name | upper }}</h1>
-            <p>{{ pokemon.type }}</p> 
-        </div>
+  <div class="card" @click="changeImage">
+    <img class="pokemon-image" :src="currentImage" />
+    <div class="container">
+      <h1>{{ name | upper }}</h1>
+      <p>{{ pokemon.type }}</p>
     </div>
+  </div>
 </template>
 
 <script>
-
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
-    created() {
-        axios.get(this.url).then(response => {
-            this.pokemon.type = response.data.types[0].type.name
-            this.pokemon.front = response.data.sprites.front_default
-            this.pokemon.back = response.data.sprites.back_default
-            this.currentImage = this.pokemon.front
-        }) 
-    },
+  created() {
+    axios.get(this.url).then((response) => {
+      this.pokemon.type = response.data.types[0].type.name;
+      this.pokemon.front = response.data.sprites.front_default;
+      this.pokemon.back = response.data.sprites.back_default;
+      this.currentImage = this.pokemon.front;
+    });
+  },
 
-    data() {
-        return {
-            isFront: true,
-            currentImage: '',
-            pokemon: {
-                type: '',
-                front: '',
-                back: ''
-            }
-        }
-    },
+  data() {
+    return {
+      isFront: true,
+      currentImage: '',
+      pokemon: {
+        type: '',
+        front: '',
+        back: '',
+      },
+    };
+  },
 
-    props: {
-        name: String,
-        url: String
-    },
+  props: {
+    name: String,
+    url: String,
+  },
 
-    filters: {
-        upper(value) {
-            let newName = value[0].toUpperCase() + value.slice(1)
-            return newName
-        }
+  filters: {
+    upper(value) {
+      let newName = value[0].toUpperCase() + value.slice(1);
+      return newName;
     },
+  },
 
-    methods: {
-        changeImage() {
-            if (this.isFront) {
-                this.isFront = false
-                this.currentImage = this.pokemon.back
-            } else {
-                this.isFront = true
-                this.currentImage = this.pokemon.front
-            }
-        }
-    }
-}
+  methods: {
+    changeImage() {
+      if (this.isFront) {
+        this.isFront = false;
+        this.currentImage = this.pokemon.back;
+      } else {
+        this.isFront = true;
+        this.currentImage = this.pokemon.front;
+      }
+    },
+  },
+};
 </script>
 
 <style lang="postcss">
-    .card {
-        @apply cursor-pointer box-border duration-300 w-40 border m-1 rounded;
-        box-shadow: 0 0 10px -8px rgba(0, 0, 0, .2);
-    }
+.card {
+  @apply cursor-pointer box-border duration-300 w-40 border m-1 rounded;
+  box-shadow: 0 0 10px -8px rgba(0, 0, 0, 0.2);
+}
 
-    .card:hover {
-        @apply border-yellow-400 transform scale-105;
-        box-shadow: 0 0 10px -2px rgba(0, 0, 0, .2);
-    }
+.card:hover {
+  @apply border-yellow-400 transform scale-105;
+  box-shadow: 0 0 10px -2px rgba(0, 0, 0, 0.2);
+}
 
-    .container {
-        @apply bg-gray-100 text-center text-gray-700 font-medium py-1 px-4;
-    }
-    
-    .pokemon-image {
-        @apply w-full;
-    }
+.container {
+  @apply bg-gray-100 text-center text-gray-700 font-medium py-1 px-4;
+}
 
-    @media only screen and (max-width: 415px) {
-        .card {
-            @apply w-32;
-        }
-    }
+.pokemon-image {
+  @apply w-full;
+}
+
+@media only screen and (max-width: 415px) {
+  .card {
+    @apply w-32;
+  }
+}
 </style>
